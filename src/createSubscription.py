c5ccc4bfe5dcf006485dd1ad0ae8f1ffdfc21b62
Ashish Akshantal <ashish.akshantal@bizcloudexperts.com>
2021-02-19 00:08:10 -0600

    exception handling completed

--- src/createSubscription.py
+++ src/createSubscription.py
@@ -2,8 +2,10 @@ import boto3
 import os
 import json
 import logging
-import botocore.session
-session = botocore.session.get_session()
+import pydash
+import validators
+import jsonschema
+from jsonschema import validate
 client = boto3.client('dynamodb')
 sns_client = boto3.client('sns')
 logger = logging.getLogger()
@@ -14,49 +16,52 @@ InternalErrorMessage = "Internal Error."
 
 def handler(event, context):
     logger.info("Event body is: {}".format(json.dumps(event['body'])))
-    customer_id = event['enhancedAuthContext']['customerId']
+
+    customer_id = event['enhancedAuthContext']['customerId']    
+    validate_input(event['body'])
+
+    endpoint = event['body']['Endpoint']
     customer_payload = event['body']
     preference = event['body']['Preference']
     event_type = event['body']['EventType']
-    if "https" not in (event['body']['Endpoint']):
-        return get_response("error", "Endpoint not accepted : Must be a only a https")
-    else:
-        response = dynamo_get(customer_id, event_type) 
-        logger.info("Dynamo get response: {}".format(json.dumps(response)))
-        
-        if len(response["Items"]) != 0:
-            return get_response("error", "Subscription already exists")
-        else:
-            update_customer_preference(customer_payload,customer_id)
+    shared_secret = event['body']['SharedSecret']
 
-        protocol = "https"            
-        endpoint = event['body']['Endpoint']
-        shared_secret = event['body']['SharedSecret']
-        
+    response = dynamo_get(customer_id, event_type) 
+    logger.info("Dynamo get response: {}".format(json.dumps(response)))
+    
+    if len(response["Items"]) != 0:
+        raise InputError(json.dumps({"httpStatus": 400, "message":"Subscription already exists"}))
+    else:
+        update_customer_preference(customer_payload,customer_id)
+    
+    try:
         events_response = client.query(TableName=os.environ['EVENTING_TOPICS_TABLE'],
                                     KeyConditionExpression='Event_Type = :Event_Type', 
                                     ExpressionAttributeValues={':Event_Type': {'S': event_type}},
                                     ProjectionExpression='Event_Payload_Topic_Arn,Full_Payload_Topic_Arn')
         logger.info("Dynamo response from eventing table: {}".format(json.dumps(events_response)))
+    except Exception as e:
+        logging.exception("EventingTopicsError: {}".format(e))
+        raise EventingTopicsError(json.dumps({"httpStatus": 501, "message": InternalErrorMessage}))
 
-        if "full" in preference:
-            full_topic_arn = events_response['Items'][0]['Full_Payload_Topic_Arn']['S']
-            full_topic_response = subscribe_to_topic(full_topic_arn,protocol,endpoint,customer_id)
-            return full_topic_response
-        else:
-            event_topic_arn = events_response['Items'][0]['Event_Payload_Topic_Arn']['S']
-            event_topic_response = subscribe_to_topic(event_topic_arn,protocol,endpoint,customer_id)
-            return event_topic_response
-
-
-def subscribe_to_topic(topic_arn,protocol,endpoint,customer_id):
+    if len(events_response["Items"]) == 0:
+        raise InputError(json.dumps({"httpStatus": 400, "message":"EventType does not exists."}))
     try:
-        response = sns_client.subscribe(
-        TopicArn=topic_arn,
-        Protocol=protocol,
-        Endpoint=endpoint,
-         Attributes={"FilterPolicy": json.dumps({"customer_id": [customer_id]})},
-        ReturnSubscriptionArn=True)
+        if preference == "fullPayload":
+            arn = events_response['Items'][0]['Full_Payload_Topic_Arn']['S']    
+            subscribe_to_topic(arn,endpoint,customer_id)            
+        else:
+            arn = events_response['Items'][0]['Event_Payload_Topic_Arn']['S']
+            subscribe_to_topic(arn,endpoint,customer_id)            
+    except Exception as e:
+        logging.exception("CreateSubScriptionError: {}".format(e))
+        raise CreateSubScriptionError(json.dumps({"httpStatus": 501, "message": InternalErrorMessage}))
+    
+def subscribe_to_topic(topic_arn,endpoint,customer_id):
+    try:
+        response = sns_client.subscribe(TopicArn=topic_arn, Protocol="https",Endpoint=endpoint,
+                                        Attributes={"FilterPolicy": json.dumps({"customer_id": [customer_id]})},
+                                        ReturnSubscriptionArn=True)
     except Exception as e:
         logging.exception("SubscribeToTopicError: {}".format(e))
         raise SubscribeToTopicError(json.dumps({"httpStatus": 501, "message": InternalErrorMessage}))
@@ -76,40 +81,57 @@ def dynamo_get(customer_id, event_type):
 
 def update_customer_preference(customer_data,customer_id):
     try:
-        event_type = customer_data['EventType']
-        subscription_preference = customer_data['Preference']
-        endpoint = customer_data['Endpoint']
-        shared_secret = customer_data['SharedSecret']
         response = client.put_item(
             TableName = os.environ['CUSTOMER_PREFERENCE_TABLE'],
             Item={
                 'Event_Type': {
-                'S': event_type
+                'S': customer_data['EventType']
                 },
                 'Subscription_Preference':{
-                'S': subscription_preference
+                'S': customer_data['Preference']
                 },
                 'Customer_Id': {
                 'S': customer_id
                 },
                 'Endpoint' :{
-                 'S': endpoint   
+                 'S': customer_data['Endpoint']   
                 },
                 'Shared_Secret' :{
-                 'S': shared_secret   
+                 'S': customer_data['SharedSecret']   
                 }
             }
         )
-        return response
     except Exception as e:
         logging.exception("UpdateCustomerPreferenceTableError: {}".format(e))
         raise UpdateCustomerPreferenceTableError(json.dumps({"httpStatus": 501, "message": InternalErrorMessage}))
 
 
-def get_response(status, msg):
-    return {"status": status, "message": msg}
-
+def validate_input(payload):
+    schema = {
+    "type" : "object",
+    "required": ["EventType",
+                  "Endpoint",
+                  "SharedSecret",
+                  "Preference"],
+    "properties" : {
+        "EventType" : {"type" : "string"}, 
+        "Endpoint" : {"type" : "string"},
+        "SharedSecret" : {"type" : "string"},
+        "Preference" : {"type" : "string",
+                        "enum" :  ["fullPayload","Change"]}
+        }}
+    try:
+        validate(instance=payload,schema=schema)
+    except jsonschema.exceptions.ValidationError as e:
+        raise InputError(json.dumps({"httpStatus": 400, "message":e.message}))
+    if not validators.url(payload['Endpoint']) or not pydash.strings.starts_with(payload['Endpoint'],"https"):
+        raise InputError(json.dumps({"httpStatus": 400, "message":"Only Valid HTTPS endpoints are accepted"}))
     
+        
+class ValidationError(Exception): pass    
+class InputError(Exception): pass    
 class UpdateCustomerPreferenceTableError(Exception): pass
 class DynamoGetError(Exception): pass
+class EventingTopicsError(Exception): pass
 class SubscribeToTopicError(Exception): pass
+class CreateSubScriptionError(Exception): pass
