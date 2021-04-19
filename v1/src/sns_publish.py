b31db5d97a3a8f342ad60bda413f27179a7290ea
Ashish Akshantal <ashish.akshantal@bizcloudexperts.com>
2021-04-18 20:41:44 -0500

    made changes to sns publish

--- v1/src/sns_publish.py
+++ v1/src/sns_publish.py
@@ -1,50 +1,49 @@
+import os
+import json
 import hashlib
 import hmac
 import io
-import json
-import boto3
-import os
 import logging
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+import boto3
+sns_client = boto3.client('sns')
 from pandas import read_csv, merge, DataFrame
 import psycopg2
-import logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-sns_client = boto3.client('sns')
 
 def handler(event, context):
-    logger.info(event)
-    if('existing' not in event):
+    LOGGER.info(event)
+    if 'existing' not in event:
         try:
             bucket = event['Records'][0]['s3']['bucket']['name']
             key = event['Records'][0]['s3']['object']['key']
-        except Exception as e:
-            logging.exception("EventObjectNameFetchError: {}".format(e))
+        except Exception as read_error:
+            logging.exception("EventObjectNameFetchError: %s", read_error)
         end = '-diff.csv000'
         event_topic = ((key.split("/dev-"))[1].split(end)[0])
         dataframe = get_s3_object(bucket, key)
-        if('shipment-info' in event_topic):
+        if 'shipment-info' in event_topic:
             diff_payload, full_payload = get_events_json_shipments(dataframe)
-            if ((diff_payload== None) and (full_payload==None)):
+            if ((diff_payload is None) and (full_payload is None)):
                 event['existing'] = 'true'
                 event['input'] = []
-                logger.info("No Valid Shipment Info")
+                LOGGER.info("No Valid Shipment Info")
                 return event
             sns_event = 'ShipmentUpdates'
-        if('customer-invoices' in event_topic):
+        if 'customer-invoices' in event_topic:
             diff_payload, full_payload = get_events_json_invoices(dataframe)
-            if ((diff_payload== None) and (full_payload==None)):
+            if ((diff_payload is None) and (full_payload is None)):
                 event['existing'] = 'true'
                 event['input'] = []
-                logger.info("No Valid Customer Invoices Info")
+                LOGGER.info("No Valid Customer Invoices Info")
                 return event
             sns_event = 'CustomerInvoices'
-        elif('shipment-milestone' in event_topic):
+        elif 'shipment-milestone' in event_topic:
             diff_payload, full_payload = get_events_json_milestones(dataframe)
-            if ((diff_payload== None) and (full_payload==None)):
+            if ((diff_payload is None) and (full_payload is None)):
                 event['existing'] = 'true'
                 event['input'] = []
-                logger.info("No Valid Shipment Milestone Info")
+                LOGGER.info("No Valid Shipment Milestone Info")
                 return event
             sns_event = 'Milestone'
         diff_list = include_shared_secret(diff_payload, 'change', sns_event)
@@ -53,7 +52,7 @@ def handler(event, context):
     else:
         merged_list = event['input']
 
-    if(len(merged_list) == 0):
+    if len(merged_list) == 0:
         event['end'] = 'true'
     else:
         count = 0
@@ -62,10 +61,10 @@ def handler(event, context):
             sns_publish(message, sns_event)
             merged_list[index]['published'] = 'true'
             count += 1
-            if (count >= 20):
+            if count >= 20:
                 break
 
-        if(len([d for d in merged_list if 'published' not in d]) == 0):
+        if len([d for d in merged_list if 'published' not in d]) == 0:
             event['end'] = 'true'
         else:
             event['end'] = 'false'
@@ -76,7 +75,7 @@ def handler(event, context):
 
 def get_events_json_shipments(dataframe):
     try:
-        logger.info("shipment-info")
+        LOGGER.info("shipment-info")
         raw_data = (dataframe.dropna(subset=['bill_to_nbr'])).fillna(value='NA')
         raw_data['bill_to_nbr'] = raw_data['bill_to_nbr'].str.strip()
         raw_data = raw_data.rename({'current_status': 'order_status'}, axis=1)
@@ -92,10 +91,10 @@ def get_events_json_shipments(dataframe):
         changed_data = changed_data.loc[changed_data['file_nbr'].isin(old_file_nbrs)]
         bill_to_numbers = raw_data.bill_to_nbr.unique()
         bill_to_numbers = tuple([i.strip() for i in bill_to_numbers])
-        if (len(bill_to_numbers)==1):
+        if len(bill_to_numbers)==1:
             bill_to_numbers = '('+bill_to_numbers[0]+')'
-        if (len(bill_to_numbers)==0):
-            logger.info("Returning No valid shipment information")
+        if len(bill_to_numbers)==0:
+            LOGGER.info("Returning No valid shipment information")
             return None, None
         old_data = old_data.set_index(['id'])
         changed_data = changed_data.set_index(['id'])
@@ -118,12 +117,12 @@ def get_events_json_shipments(dataframe):
         json_obj = json.loads(final_diff.apply(lambda x: [x.dropna()], axis=1).to_json())
         json_obj_full = json.loads(full_payload.to_json(orient='records'))
         return json_obj, json_obj_full
-    except Exception as e:
-        logging.exception("ShipmentInfoJSONError: {}".format(e))
+    except Exception as shipment_info_json_error:
+        logging.exception("ShipmentInfoJSONError:  %s", shipment_info_json_error)
 
 def get_events_json_invoices(dataframe):
     try:
-        logger.info("customer-invoices")
+        LOGGER.info("customer-invoices")
         raw_data = (dataframe.dropna(subset=['bill_to_nbr'])).fillna(value='NA')
         raw_data['bill_to_nbr'] = raw_data['bill_to_nbr'].str.strip()
         old_data = raw_data.loc[raw_data['record_type'] == 'OLD']
@@ -134,10 +133,10 @@ def get_events_json_invoices(dataframe):
         changed_data = changed_data.loc[changed_data['file_nbr'].isin(old_file_nbrs)]
         bill_to_numbers = raw_data.bill_to_nbr.unique()
         bill_to_numbers = tuple([i.strip() for i in bill_to_numbers])
-        if (len(bill_to_numbers)==1):
+        if len(bill_to_numbers)==1:
             bill_to_numbers = '('+bill_to_numbers[0]+')'
-        if (len(bill_to_numbers)==0):
-            logger.info("Returning No valid Customer Invoices details")
+        if len(bill_to_numbers)==0:
+            LOGGER.info("Returning No valid Customer Invoices details")
             return None, None
         old_data = old_data.set_index(['id'])
         changed_data = changed_data.set_index(['id'])
@@ -159,11 +158,11 @@ def get_events_json_invoices(dataframe):
         json_obj = json.loads(final_diff.apply(lambda x: [x.dropna()], axis=1).to_json())
         json_obj_full = json.loads(full_payload.to_json(orient='records'))
         return json_obj, json_obj_full
-    except Exception as e:
-        logging.exception("CustomerInvoicesJSONError: {}".format(e))
+    except Exception as customer_invoices_json_error:
+        logging.exception("CustomerInvoicesJSONError: %s", customer_invoices_json_error)
 def get_events_json_milestones(dataframe):
     try:
-        logger.info("shipment-milestones")
+        LOGGER.info("shipment-milestones")
         raw_data = (dataframe.dropna(subset=['bill_to_nbr'])).fillna(value='NA')
         indexNames = raw_data[ raw_data['source_system'] == 'EE' ].index
         raw_data.drop(indexNames , inplace=True)
@@ -179,11 +178,11 @@ def get_events_json_milestones(dataframe):
         changed_data = changed_data.loc[changed_data['file_nbr'].isin(old_file_nbrs)]
         bill_to_numbers = raw_data.bill_to_nbr.unique()
         bill_to_numbers = tuple([i.strip() for i in bill_to_numbers])
-        if (len(bill_to_numbers)==1):
+        if len(bill_to_numbers)==1:
             bill_to_numbers = '('+bill_to_numbers[0]+')'
-        if (len(bill_to_numbers)==0):
-            logger.info("Returning No valid Shipment Milestone Details")
-            return None, None    
+        if len(bill_to_numbers)==0:
+            LOGGER.info("Returning No valid Shipment Milestone Details")
+            return None, None
         changed_data = changed_data.set_index(['id'])
         new_data = new_data.set_index(['id'])
         changed_data = changed_data.sort_index()
@@ -205,16 +204,16 @@ def get_events_json_milestones(dataframe):
         json_obj = json.loads(final_diff.apply(lambda x: [x.dropna()], axis=1).to_json())
         json_obj_full = json.loads(full_payload.to_json(orient='records'))
         return json_obj, json_obj_full
-    except Exception as e:
-        logging.exception("ShipmentsMilestonesJSONError: {}".format(e))
+    except Exception as shipment_milestone_json_error:
+        logging.exception("ShipmentsMilestonesJSONError: %s", shipment_milestone_json_error)
 def get_s3_object(bucket, key):
     try:
         client = boto3.client('s3')
         response = client.get_object(Bucket=bucket, Key=key)
         dataframe = read_csv(io.BytesIO(response['Body'].read()), dtype=str)
         return dataframe
-    except Exception as e:
-        logging.exception("S3GetObjectError: {}".format(e))
+    except Exception as get_error:
+        logging.exception("S3GetObjectError: %s", get_error)
 
 def merge_rawdata_with_x12codes(raw_data, order_status_x12_ref_df):
     try:
@@ -222,8 +221,8 @@ def merge_rawdata_with_x12codes(raw_data, order_status_x12_ref_df):
                                 right_on=['omni_cd'])
         raw_data_with_x12 = raw_data_with_x12.drop(columns=['order_status', 'omni_cd'])
         return raw_data_with_x12
-    except Exception as e:
-        logging.exception("RawDataX12CodesMergeError: {}".format(e))
+    except Exception as merge_error:
+        logging.exception("RawDataX12CodesMergeError: %s", merge_error)
 
 def get_x12_codes(order_status):
     try:
@@ -234,13 +233,14 @@ def get_x12_codes(order_status):
         cur = con.cursor()
         cur.execute(f"select x12_cd, x12_event_desc, omni_cd from public.x12_cross_ref where omni_cd in {order_status}")
         con.commit()
-        x = cur.fetchall()
-        x12_codes_df = DataFrame.from_records(x, columns=['x12_cd', 'x12_event_desc', 'omni_cd'])
+        edi_codes = cur.fetchall()
+        x12_codes_df = DataFrame.from_records(edi_codes, columns=['x12_cd', 'x12_event_desc', 'omni_cd'])
         cur.close()
         con.close()
         return x12_codes_df
-    except Exception as e:
-        logging.exception("GetX12CodesError: {}".format(e))
+    except Exception as get_code_error:
+        logging.exception("GetX12CodesError: %s", get_code_error)
+
 def get_shared_secret(cust_id, event_type):
     try:
         cust_id = str(cust_id)
@@ -249,10 +249,10 @@ def get_shared_secret(cust_id, event_type):
             Key={'Customer_Id': {'S': cust_id}, 'Event_Type': {'S': event_type}})
         if 'Item' in response:
             return response['Item']['Shared_Secret']['S']
-        else:
-            return None
-    except Exception as e:
-        logging.exception("DynamoDBCQueryExecutionError: {}".format(e))
+        return None
+    except Exception as dynamo_error:
+        logging.exception("DynamoDBCQueryExecutionError: %s", dynamo_error)
+
 def get_cust_id(bill_to_numbers):
     con = psycopg2.connect(dbname=os.environ['DBNAME'],
                            host=os.environ['HOST'],
@@ -261,8 +261,8 @@ def get_cust_id(bill_to_numbers):
     cur = con.cursor()
     cur.execute(f"select id, cust_nbr, source_system from public.api_token where cust_nbr in {bill_to_numbers}")
     con.commit()
-    x = cur.fetchall()
-    cust_id_df = DataFrame.from_records(x, columns=['customer_id', 'bill_to_nbr', 'source_system'])
+    api_token_results = cur.fetchall()
+    cust_id_df = DataFrame.from_records(api_token_results, columns=['customer_id', 'bill_to_nbr', 'source_system'])
     cur.close()
     con.close()
     return cust_id_df
@@ -274,12 +274,12 @@ def get_topic_arn(event_type):
         change_topic_arn = response['Item']['Event_Payload_Topic_Arn']['S']
         full_topic_arn = response['Item']['Full_Payload_Topic_Arn']['S']
         return change_topic_arn, full_topic_arn
-    except Exception as e:
-        logging.exception("PublishARNFetchError: {}".format(e))
+    except Exception as get_error:
+        logging.exception("PublishARNFetchError: %s", get_error)
 
 def sns_publish(message, sns_event):
     try:
-        change_topic_arn, full_topic_arn = get_topic_arn(sns_event)        
+        change_topic_arn, full_topic_arn = get_topic_arn(sns_event)
         if message["SNS_FLAG"]=="DIFF":
             topic_arn = change_topic_arn
         elif message["SNS_FLAG"]=="FULL":
@@ -293,8 +293,9 @@ def sns_publish(message, sns_event):
                                 'DataType': 'String',
                                 'StringValue': customer_id
                             }})
-    except Exception as e:
-        logging.exception("SNSPublishError: {}".format(e))
+    except Exception as publish_error:
+        logging.exception("SNSPublishError: %s", publish_error)
+
 def include_shared_secret(payload, payload_type, event_type):
     json_list = []
     try:
@@ -311,9 +312,10 @@ def include_shared_secret(payload, payload_type, event_type):
                 record['Signature'] = hmac.new(shared_secret, json_body, hashlib.sha256).hexdigest()
                 json_list.append(record)
         return json_list
-    except Exception as e:
-        logging.exception("PublishMessageError: {}".format(e))
+    except Exception as message_error:
+        logging.exception("PublishMessageError: %s", message_error)
         raise SharedSecretFetchError
+
 def merge_rawdata_with_customer_id(raw_data, cust_id_df):
     try:
         raw_data_with_cid = merge(raw_data, cust_id_df, how='inner', left_on=['bill_to_nbr', 'source_system'],
@@ -323,7 +325,8 @@ def merge_rawdata_with_customer_id(raw_data, cust_id_df):
         raw_data_with_cid = raw_data_with_cid.set_index(['id'])
         raw_data_with_cid = raw_data_with_cid.sort_index()
         return raw_data_with_cid
-    except Exception as e:
-        logging.exception("RawDataCustomerIDMergeError: {}".format(e))
+    except Exception as merge_error:
+        logging.exception("RawDataCustomerIDMergeError: %s", merge_error)
+
 class SharedSecretFetchError(Exception):
     pass
